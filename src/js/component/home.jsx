1fdc0b6be4ad3203b07f779e4986f28637da07fa
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-01-09 20:47:01 +0000

    Add bonus functionalites

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,11 @@ const Home = () => {
 	const [color1,setColor1]= useState("");
 	const [color2,setColor2]= useState("");
 	const [color3,setColor3]= useState("");
+	const [display,setDisplay]= useState("d-none");
+	const [color4,setColor4]= useState("");
+	const [margenbt,setMargen]= useState("mb-2");
 	
+
 	function encenderRojo() {
 		if(color1 === "0 0 10px 13px #f9e82c"){
 			setColor1("");
@@ -13,6 +17,7 @@ const Home = () => {
 			setColor1("0 0 10px 13px #f9e82c");
 			setColor2("");
 			setColor3("");
+			setColor4("");
 		}
 	}
 	function encenderAmarillo() {
@@ -22,6 +27,7 @@ const Home = () => {
 			setColor2("0 0 10px 13px #f9e82c");
 			setColor1("");
 			setColor3("");
+			setColor4("");
 		}
 	}
 	function encenderVerde() {
@@ -31,6 +37,7 @@ const Home = () => {
 			setColor3("0 0 10px 13px #f9e82c");
 			setColor1("");
 			setColor2("");
+			setColor4("");
 		}
 	}
 	function alternarColor() {
@@ -38,19 +45,53 @@ const Home = () => {
 			setColor1("");
 			setColor2("0 0 10px 13px #f9e82c");
 			setColor3("");
+			setColor4("");
 		}
 		if(color2 === "0 0 10px 13px #f9e82c"){
 			setColor1("");
 			setColor2("");
 			setColor3("0 0 10px 13px #f9e82c");
+			setColor4("");
 		}
 		if(color3 === "0 0 10px 13px #f9e82c"){
+			if(display === "d-none"){
+				setColor1("0 0 10px 13px #f9e82c");
+				setColor2("");
+				setColor3("");
+			} else{
+				setColor1("");
+				setColor2("");
+				setColor3("");
+				setColor4("0 0 10px 13px #f9e82c");}
+		}
+		if(color4 === "0 0 10px 13px #f9e82c"){
+			setColor1("0 0 10px 13px #f9e82c");
 			setColor2("");
 			setColor3("");
+			setColor4("");
+		}
+		else if(color1 ==="" && color2 ==="" && color3 ==="" && color4 ===""){
 			setColor1("0 0 10px 13px #f9e82c");
 		}
-		else if(color1 ==="" && color2 ==="" && color3 ===""){
-			setColor1("0 0 10px 13px #f9e82c");
+	}
+
+	function desplegarVioleta() {
+		if(display === "d-none"){
+			setDisplay("");
+			setMargen("");
+		}else{
+			setDisplay("d-none");
+			setMargen("mb-2");
+		}
+	}
+	function encenderVioleta() {
+		if(color4 === "0 0 10px 13px #f9e82c"){
+			setColor4("");
+		}else{
+			setColor4("0 0 10px 13px #f9e82c");
+			setColor1("");
+			setColor2("");
+			setColor3("");
 		}
 	}
 	return (
@@ -61,9 +102,11 @@ const Home = () => {
 			<div class="d-grid gap-0 col-1 mx-auto bg-dark rounded-4">
   				<button className={"btn mt-2 mx-2 rounded-circle btn-danger"} onClick={encenderRojo} style={{height: "100px", boxShadow: color1}}></button>
   				<button className={"btn mx-2 rounded-circle btn-warning"} onClick={encenderAmarillo} style={{height: "100px", boxShadow: color2}}></button>
-				<button className={"btn mb-2 mx-2 rounded-circle btn-success"} onClick={encenderVerde} style={{height: "100px", boxShadow: color3}}></button>
+				<button className={"btn mx-2 rounded-circle btn-success " + margenbt} onClick={encenderVerde} style={{height: "100px", boxShadow: color3}}></button>
+				<button className={"btn mb-2 mx-2 rounded-circle " + display} onClick={encenderVioleta} style={{height: "100px", backgroundColor: "purple", boxShadow: color4}}></button>
 			</div>
 			<button type="button" className="btn btn-info m-5" onClick={alternarColor}>Click to change light</button>
+			<button type="button" className="btn btn-info m-5" onClick={desplegarVioleta}>Click to add blue light</button>
 		</div>
 	);
 };
